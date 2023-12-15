69941a0bfe8ef8e0ed24484d042b4a5ffc9da172
Maximiliano Cóppola <maxicopp@outlook.com>
2023-12-15 01:53:32 -0300

    Working with object types

--- 01-ts-refresher/first-app.ts
+++ 01-ts-refresher/first-app.ts
@@ -13,3 +13,21 @@ let userID: string | number = 'abc123';
 userID = 123;
 
 // userID = true;
+// let user: object;
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+// user = 'Max';
+
+user = {
+  name: 'Max',
+  age: 27,
+  isAdmin: true,
+  id: 'abc', // 123
+};
+
+// user = {};
