d90683b12faeeaffd54192ec32c4d42c770d18ac
Maximiliano Cóppola <maxicopp@outlook.com>
2023-12-17 22:07:08 -0300

    Defining function types

--- 01-ts-refresher/first-app.ts
+++ 01-ts-refresher/first-app.ts
@@ -43,3 +43,13 @@ function add(a: number, b: number): number {
   const result = a + b;
   return result;
 }
+
+function calculate(
+  a: number,
+  b: number,
+  calcFn: (a: number, b: number) => number
+) {
+  calcFn(a, b);
+}
+
+calculate(2, 3, add);
