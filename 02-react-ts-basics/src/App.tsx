988f9f6c91f0a0093787e446021158538999613e
Maximiliano Cóppola <maxicopp@outlook.com>
2023-12-22 01:50:41 -0300

    Defining components props types

--- 02-react-ts-basics/src/App.tsx
+++ 02-react-ts-basics/src/App.tsx
@@ -3,7 +3,10 @@ import CourseGoal from './components/CourseGoal';
 function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal
+        title="Learn React + TS"
+        description="Learn it from the ground up"
+      />
     </main>
   );
 }
--- 02-react-ts-basics/src/components/CourseGoal.tsx
+++ 02-react-ts-basics/src/components/CourseGoal.tsx
@@ -1,9 +1,14 @@
-export default function CourseGoal() {
+interface Props {
+  title: string;
+  description: string;
+}
+
+export default function CourseGoal({ title, description }: Props) {
   return (
     <article>
       <div>
-        <h2>TITLE</h2>
-        <p>DESCRIPTION</p>
+        <h2>{title}</h2>
+        <p>{description}</p>
       </div>
       <button>DELETE</button>
     </article>
