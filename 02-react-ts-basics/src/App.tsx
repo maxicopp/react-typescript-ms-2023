a6fe5c1f750f1a1d2c95da58ac2790386141f395
Maximiliano Cóppola <maxicopp@outlook.com>
2024-01-03 04:31:02 -0300

    Using useState and TypeScript

--- 02-react-ts-basics/src/App.tsx
+++ 02-react-ts-basics/src/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import CourseGoal from './components/CourseGoal';
 import goalsImg from './assets/goals.jpg';
 import Header from './components/Header';
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal() {
+    console.log('Add goal');
+    setGoals((prevGoals) => [
+      ...prevGoals,
+      {
+        title: 'Learn React + TS',
+        description: 'Learn it from the ground up',
+        id: Math.random(),
+      },
+    ]);
+  }
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add New Goal</button>
       <CourseGoal title="Learn React + TS">
         <p>Learn it from the ground up</p>
       </CourseGoal>
