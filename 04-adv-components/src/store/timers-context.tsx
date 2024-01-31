55885aefff1c61504868e5e22b9d7b8afb307131
Maximiliano Cóppola <maxicopp@outlook.com>
2024-01-31 02:27:44 -0300

    Using better action types

--- 04-adv-components/src/store/timers-context.tsx
+++ 04-adv-components/src/store/timers-context.tsx
@@ -37,17 +37,62 @@ type TimerContextProviderProps = {
   children: ReactNode;
 };
 
+type StartTimerAction = {
+  type: 'START_TIMER';
+};
+
+type StopTimerAction = {
+  type: 'STOP_TIMER';
+};
+
+type AddTimerAction = {
+  type: 'ADD_TIMER';
+  payload: Timer;
+};
+
+type Action = StartTimerAction | StopTimerAction | AddTimerAction;
+
+function timersReducer(state: TimersState, action: Action): TimersState {
+  switch (action.type) {
+    case 'ADD_TIMER':
+      return {
+        ...state,
+        timers: action.payload ? [...state.timers, action.payload] : [],
+      };
+    case 'START_TIMER':
+      return {
+        ...state,
+        isRunning: true,
+      };
+    case 'STOP_TIMER':
+      return {
+        ...state,
+        isRunning: false,
+      };
+    default:
+      return state;
+  }
+}
+
 export default function TimersContextProvider({
   children,
 }: TimerContextProviderProps) {
-  const [ctxState, dispatch] = useReducer(reducer, initialState);
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
   const ctx: TimersContextValue = {
-    isRunning: false,
-    timers: [],
-    addTimer: () => {},
-    startTimer: () => {},
-    stopTimer: () => {},
+    isRunning: timersState.isRunning,
+    timers: timersState.timers,
+    addTimer: (timerData) => {
+      dispatch({ type: 'ADD_TIMER', payload: timerData });
+    },
+    startTimer: () => {
+      dispatch({ type: 'START_TIMER' });
+    },
+    stopTimer: () => {
+      dispatch({ type: 'STOP_TIMER' });
+    },
   };
+
   return (
     <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
   );
