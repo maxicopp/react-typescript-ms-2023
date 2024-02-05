df5cbee7ebe721847d7090ff5c82cff81f5cd70e
Maximiliano Cóppola <maxicopp@outlook.com>
2024-02-05 04:01:04 -0300

    Fetching & transforming data

--- 05-data-fetching/src/App.tsx
+++ 05-data-fetching/src/App.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from 'react';
+import { get } from './util/http';
+import BlogPosts, { type BlogPost } from './components/BlogPosts';
+import fetchingImg from './assets/data-fetching.png';
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await get<BlogPost[]>(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+
+      setFetchedPosts(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fetching process."
+      />
+      {fetchedPosts && <BlogPosts posts={fetchedPosts} />}
+    </main>
+  );
 }
 
 export default App;
--- 05-data-fetching/src/util/http.ts
+++ 05-data-fetching/src/util/http.ts
@@ -1,10 +1,10 @@
-export async function get(url: string) {
+export async function get<T>(url: string): Promise<T> {
   const response = await fetch(url);
 
   if (!response.ok) {
     throw new Error('Failed to fetch data.');
   }
 
-  const data = (await response.json()) as unknown;
+  const data = (await response.json()) as T;
   return data;
 }
