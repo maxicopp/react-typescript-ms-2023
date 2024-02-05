d2da66381cda63773f3d45a620bf9dcfcf141787
Maximiliano Cóppola <maxicopp@outlook.com>
2024-02-05 04:22:47 -0300

    Using the zod library for response data validation

--- 05-data-fetching/src/App.tsx
+++ 05-data-fetching/src/App.tsx
@@ -2,26 +2,47 @@ import { useEffect, useState } from 'react';
 import { get } from './util/http';
 import BlogPosts, { type BlogPost } from './components/BlogPosts';
 import fetchingImg from './assets/data-fetching.png';
+import { z } from 'zod';
+
+const BlogPostSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+  text: z.string(),
+});
+const BlogPostArraySchema = z.array(BlogPostSchema);
+
+type BlogResponse = {
+  id: number;
+  title: string;
+  body: string;
+};
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
 
   useEffect(() => {
     async function fetchData() {
-      const data = await get<BlogPost[]>(
-        'https://jsonplaceholder.typicode.com/posts'
-      );
-
-      setFetchedPosts(data);
+      try {
+        const rawData = await get<BlogResponse[]>('https://jsonplaceholder.typicode.com/posts');
+        const validatedResponse = BlogPostArraySchema.parse(rawData.map(post => ({
+          id: post.id,
+          title: post.title,
+          text: post.body,
+        })));
+        setFetchedPosts(validatedResponse);
+      } catch (error) {
+        console.error(error);
+      }
     }
     fetchData();
   }, []);
 
   return (
     <main>
+      <h1>Data Fetching</h1>
       <img
         src={fetchingImg}
-        alt="An abstract image depicting a data fetching process."
+        alt="A depiction of data fetching."
       />
       {fetchedPosts && <BlogPosts posts={fetchedPosts} />}
     </main>
