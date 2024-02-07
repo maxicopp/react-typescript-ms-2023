a8868f2f56ad18ba199f1b48f332b185212f64ac
Maximiliano Cóppola <maxicopp@outlook.com>
2024-02-07 03:34:01 -0300

    Selecting redux data

--- 06-redux/src/store/hooks.ts
+++ 06-redux/src/store/hooks.ts
@@ -1,4 +1,9 @@
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from './store';
+import {
+  useDispatch,
+  useSelector,
+  type TypedUseSelectorHook,
+} from 'react-redux';
+import { AppDispatch, RootState } from './store';
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
